a16b5ed4bb549d1f8e09e63707ff86b679c680d9
Sean Kibbee <seankibbee@gmail.com>
2018-07-04 13:12:13 -0400

    t

--- src/app/main.ts
+++ src/app/main.ts
@@ -6,7 +6,7 @@ import { enableProdMode } from "@angular/core";
 const pageUrl = window.location.href.toLowerCase();
 
 //### the 60002 check is temporary, Ionic view app uses localhost address with 60002 port.
-if (pageUrl.indexOf("localhost") === -1 && pageUrl.indexOf("60002") === -1) {
+if (pageUrl.indexOf("localhost") === -1 || pageUrl.indexOf("60002") > -1) {
   enableProdMode();
   alert('prod mode enabled');
 } else {
