c8e1201ceab297f969f07c3a14718a959eb09701
Sean Kibbee <seankibbee@gmail.com>
2018-07-04 12:54:40 -0400

    checking dev mode properties

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,5 +1,13 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app.module';
-
 platformBrowserDynamic().bootstrapModule(AppModule);
+import { enableProdMode } from "@angular/core";
+
+const pageUrl = window.location.href.toLowerCase();
+
+if (pageUrl.indexOf("localhost") === -1) {
+  enableProdMode();
+  alert('prod mode enabled');
+} else {
+  alert('dev mode left');
+}
